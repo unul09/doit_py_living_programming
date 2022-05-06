e4dda69313e74657ca3902d500f852e01aae8e6e
unul09 <morning20404@gmail.com>
2022-05-07 01:10:22 +0900

    03. 텍스트 파일 가공하기 - rick and morty script 가공/추출 실습

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-# 02. 서비스 가격 출력 프로그램
-def service_price():
-    service = input('서비스 종류를 입력하세요, a/b/c : ')
-    valueAdded = input('부가세를 포함합니까? y/n : ')
-    if service == 'a': result = 23
-    elif service == 'b': result = 40
-    elif service == 'c': result = 67
+import os, re
 
-    if valueAdded == 'y': result = result*1.1
+pattern = r'is'
+script = '1 is one in english'
 
-    print(round(result, 1), '만 원입니다.')
-
-
-service_price()
+#re.match(pattern, script).group()
+re.search(pattern, script).group()
